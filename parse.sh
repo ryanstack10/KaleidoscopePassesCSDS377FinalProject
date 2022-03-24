632d1f117359c59a2e8ad433e74007876e0e7843
Aaron Orenstein <aao62@case.edu>
2022-03-24 00:55:34 -0400

    Updated readme

---
+++ parse.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+if [ "$#" -lt 1 ]; then
+	echo Needs one argument
+	exit 1
+fi
+
+for ARG in $@
+do
+	echo "\$cat $ARG | ./toy"
+	cat $ARG | ./toy
+
+	OUTPUT=${ARG%.*}_output.o
+	echo "\n\$mv output.o $OUTPUT"
+	mv output.o $OUTPUT
+	echo "Moved output to $OUTPUT"
+done
